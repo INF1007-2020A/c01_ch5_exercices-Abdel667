548092bda2f99c1918af6b8fcd5186b5f98728bf
Abdel667 <71038391+Abdel667@users.noreply.github.com>
2020-10-16 16:11:30 -0400

    Done

--- exercice.py
+++ exercice.py
@@ -6,25 +6,60 @@ from typing import List
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    Nb = float(input('entrez un nombre'))
+    if Nb <= 0 :
+        Nb *= (-1)
+    
+    return Nb
 
 
 def use_prefixes() -> List[str]:
+    Chaine = ''
     prefixes, suffixes = 'JKLMNOP', 'ack'
+    for letter in prefixes :
+        Chaine += letter + suffixes + ' '
+    
+    return Chaine
 
-    return [""]
+
 
 
 def prime_integer_summation() -> int:
-    return 0
+    somme = 0
+    terme=1
+    nombre = 2
+    while terme < 101:
+        
+        prime = True
+        for i in range (2, int(nombre**0.5)+1):
+            if nombre% i == 0 :
+                prime = False
+        if prime :
+            
+            somme += nombre
+            terme+= 1
+        nombre += 1
+            
+
+    return somme 
 
 
 def factorial(number: int) -> int:
-    return 0
+    factor = 1
+    for i in range (1, number):
+        factor *= i
+    factorial = number * factor
+    return factorial
 
 
 def use_continue() -> None:
-    pass
+    for i in range (1, 11):
+        if i == 5 :
+            continue
+        print(i)
+    return i
+    
+    
 
 
 def main() -> None:
@@ -43,3 +78,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
